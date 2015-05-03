1826defdbb8b10dd61bc6e5ea76ba538cfba89e4
taivop <tpungas@ut.ee>
2015-05-03 16:22:03 +0300

    proportion funded graphs

---
+++ prop_funded.R
@@ -0,0 +1,95 @@
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(reshape2)
+library(RColorBrewer)
+library(extrafont)
+
+
+data = read.table("data/cleaned.csv") 
+
+# What % of the application was funded - VERIFICATION
+data2 = data %>%
+  mutate(FundedAmount = ifelse(is.na(FundedAmount), 0, FundedAmount)) %>%
+  mutate(funded_perc=FundedAmount/AppliedAmount) %>%
+  mutate(VerificationType=mapvalues(VerificationType, from=1:4, to=
+                                  c("Sissetulekut ei kontrollitud",
+                                    "Sissetulekut ei kontrollitud, kinnitati telefoni teel",
+                                    "Sissetulekut kontrolliti",
+                                    "Sissetulekut ja kulusid kontrolliti"))) %>%
+  group_by(credit_score, VerificationType) %>%
+  summarise(mean_funding=mean(funded_perc), count=n()) %>%
+  na.omit() %>%
+  filter(credit_score > 0)
+
+# Plot
+ggplot(data2) +
+  geom_bar(aes(x=credit_score, y=mean_funding, fill=VerificationType), stat="identity") +
+  scale_x_continuous(breaks=seq(500,1000,100)) +
+  xlab("Krediidiskoor") +
+  ylab("Rahuldatud taotluste osakaal") + 
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        text=element_text(size=16, family="Open Sans"), legend.position="none") +
+  facet_wrap(~ VerificationType)
+
+
+
+# What % of the application was funded - COUNTRY
+data3 = data %>%
+  mutate(FundedAmount = ifelse(is.na(FundedAmount), 0, FundedAmount)) %>%
+  mutate(funded_perc=FundedAmount/AppliedAmount) %>%
+  group_by(credit_score, Country) %>%
+  summarise(mean_funding=mean(funded_perc), count=n()) %>%
+  na.omit() %>%
+  filter(credit_score > 0)
+
+# Plot
+ggplot(data3) +
+  geom_bar(aes(x=credit_score, y=mean_funding, fill=Country), stat="identity") +
+  scale_x_continuous(breaks=seq(500,1000,100)) +
+  xlab("Krediidiskoor") +
+  ylab("Rahuldatud taotluste osakaal") + 
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        text=element_text(size=16, family="Open Sans"), legend.position="none") +
+  facet_wrap(~ Country)
+
+
+
+
+
+
+# What % of the application was funded - LANGUAGE
+data4 = data %>%
+  mutate(FundedAmount = ifelse(is.na(FundedAmount), 0, FundedAmount)) %>%
+  mutate(funded_perc=FundedAmount/AppliedAmount) %>%
+  mutate(Language=mapvalues(language_code, from=1:7, to=
+                                      c("eesti",
+                                        "inglise",
+                                        "vene",
+                                        "soome",
+                                        "saksa",
+                                        "hispaania",
+                                        "slovakkia"))) %>%
+  filter(language_code <= 7 & !(language_code %in% c(5,7))) %>% # saksa ja slovakkia keeles vähe värki
+  group_by(credit_score, Language) %>%
+  summarise(mean_funding=mean(funded_perc), count=n()) %>%
+  na.omit() %>%
+  filter(credit_score > 0)
+
+# Plot
+ggplot(data4) +
+  geom_bar(aes(x=credit_score, y=mean_funding, fill=Language), stat="identity") +
+  scale_x_continuous(breaks=seq(500,1000,100)) +
+  xlab("Krediidiskoor") +
+  ylab("Rahuldatud taotluste osakaal") + 
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        text=element_text(size=16, family="Open Sans"), legend.position="none") +
+  facet_wrap(~ Language) +
+  scale_colour_brewer(palette="Set1")
+
+# RESULT: biases in investors? Why don't Spanish high-rated lenders get as much money as in Estonia? Interest rate should cover this issue, right?
+
